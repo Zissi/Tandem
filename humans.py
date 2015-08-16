2ea21a5c8ef0f5d119a8889336d7e38ec920e7d3
DerDot <wahlholz@gmx.de>
2015-08-16 15:18:51 +0200

    Use conditions in ILP solver

--- humans.py
+++ humans.py
@@ -1,6 +1,9 @@
-class Humans(object):
+class Human(object):
 
     def __init__(self, name, learning_languages, teaching_languages):
         self.name = name
         self.learning_languages = learning_languages
         self.teaching_languages = teaching_languages
+
+    def __repr__(self):
+        return self.name
--- tandem.py
+++ tandem.py
@@ -1,16 +1,17 @@
 from itertools import product
 import pulp
+from humans import Human
 
-TEACHING_LANGUAGES = 'teaching_languages'
-LEARNING_LANGUAGES = 'learning_languages'
+MAX_TABLE_SIZE = 4
 
-humans = [{'name': 'tim', LEARNING_LANGUAGES: [('german', 1), ('english', 2)], TEACHING_LANGUAGES: ['french']},
-          {'name': 'tom', LEARNING_LANGUAGES: [('german', 1), ('english', 2)], TEACHING_LANGUAGES: ['french']},
-          {'name': 'tum', LEARNING_LANGUAGES: [('french', 1), ('english', 2)], TEACHING_LANGUAGES: ['german']}]
+humans = [Human(name='tim', learning_languages=[('german', 1), ('english', 2)], teaching_languages=['french']),
+          Human(name='tom', learning_languages=[('english', 1), ('french', 2)], teaching_languages=['german']),
+          Human(name='tum', learning_languages=[('german', 1), ('english', 2)], teaching_languages=['french']),
+          Human(name='tam', learning_languages=[('german', 1), ('english', 2)], teaching_languages=['french'])]
+
 
 max_tables = 5
 max_table_size = 4
-guests = 'A B C D E F G'.split()
 
 def table_languages(table):
     language_combinations = get_overlapping_languages(table)
@@ -23,7 +24,7 @@ def languages_with_teachers(table, language_combinations):
 
     for combination in language_combinations:
         if _combination_has_teachers(table, combination):
-            possible_languages += combination
+            possible_languages.add(combination)
 
     return possible_languages
 
@@ -32,7 +33,7 @@ def _combination_has_teachers(table, combination):
     has_teacher = {}
 
     for human in table:
-        for language in human[TEACHING_LANGUAGES]:
+        for language in human.teaching_languages:
             has_teacher[language] = True
             if _combination_has_enough_teachers(has_teacher, combination):
                 return True
@@ -60,8 +61,8 @@ def get_overlapping_languages(table):
 
 
 def _get_language_combinations(human):
-    learning_languages = (language for language, _ in human[LEARNING_LANGUAGES])
-    language_combinations = product(learning_languages, human[TEACHING_LANGUAGES])
+    learning_languages = (language for language, _ in human.learning_languages)
+    language_combinations = product(learning_languages, human.teaching_languages)
     return {frozenset(combination) for combination in language_combinations}
 
 
@@ -71,12 +72,12 @@ def happiness(table):
     Find the happiness of the table
     - by calculating the maximum distance between the letters
     """
-    return abs(ord(table[0]) - ord(table[-1]))
+
+    return 2
 
 # create list of all possible tables
 print('find allcombinations')
-possible_tables = [tuple(c) for c in pulp.allcombinations(guests,
-                                        max_table_size) if len(c) > 1]
+possible_tables = [table for table in pulp.allcombinations(humans, MAX_TABLE_SIZE) if len(table) > 1 and table_languages(table)]
 print('found all combinations')
 
 # create a binary variable to state that a table setting is used
@@ -85,22 +86,19 @@ x = pulp.LpVariable.dicts('table', possible_tables,
                             upBound=1,
                             cat=pulp.LpInteger)
 
-seating_model = pulp.LpProblem("Wedding Seating Model", pulp.LpMinimize)
+seating_model = pulp.LpProblem("Tandem Seating Model", pulp.LpMinimize)
 
 seating_model += sum([happiness(table) * x[table] for table in possible_tables])
 
 # specify the maximum number of tables
 seating_model += sum([x[table] for table in possible_tables]) <= max_tables, \
                             "Maximum_number_of_tables"
-
-seating_model += sum([x[table] for table in possible_tables]) == sum([x[table] for table in possible_tables]), \
-                            "test"
 print('Have model')
 
 # A guest must seated at one and only one table
-for guest in guests:
+for human in humans:
     seating_model += sum([x[table] for table in possible_tables
-                                if guest in table]) == 1, "Must_seat_%s" % guest
+                                if human in table]) == 1, "Must_seat_%s" % human
 
 print('done with model')
 
@@ -108,5 +106,6 @@ seating_model.solve()
 
 print("The choosen tables are out of a total of %s:" % len(possible_tables))
 for table in possible_tables:
+    print(x[table].value())
     if x[table].value() == 1.0:
         print(table)
