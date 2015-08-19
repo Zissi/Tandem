5cc333a0215b86d81df21465db8917f64e330679
Francisca Schlesinger <francisca.schlesinger@momox.biz>
2015-08-19 17:17:34 +0200

    Fix forms

--- hello.py
+++ hello.py
@@ -31,8 +31,6 @@ def my_form_post():
         return enter_new_human(request)
     elif request.form['btn'] == 'DeleteAll':
         return delete_all_humans()
-    elif request.form['btn'] == 'Calculate':
-        return redirect('/results')
 
 
 
