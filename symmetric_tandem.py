834f29b0d369c721064d188cdcaa3ebf91344751
Dominik <wahlholz@gmx.de>
2015-09-12 13:51:57 +0200

    Initial refactoring.

---
+++ symmetric_tandem.py
@@ -0,0 +1,149 @@
+from itertools import product
+
+import numpy as np
+import pulp
+
+from tandem import HUMANS, MAX_TABLE_SIZE, MAX_DIFFERENCE, Seater
+
+class SymmetricSeater(Seater):
+    
+    def _filtered_tables(self):
+        for table in self._tables():
+            languages = _table_languages(table)
+            for language_combination in languages:
+                if _acceptable_level_difference(table, language_combination):
+                    yield (table, frozenset(language_combination))
+                    
+    def _optimal_seatings(self):
+        possible_tables = list(self._filtered_tables())
+        is_seated = pulp.LpVariable.dicts('table',
+                                          possible_tables,
+                                          lowBound=0,
+                                          upBound=1,
+                                          cat=pulp.LpInteger)
+        seating_model = make_ilp_model(self.humans, is_seated, possible_tables)
+        seating_model.solve()
+        
+        return optimized_tables(possible_tables, is_seated)
+    
+
+def _table_languages(table):
+    language_combinations = _get_overlapping_languages(table)
+    valid_languages = _languages_with_teachers(table, language_combinations)
+    return valid_languages
+
+
+def _languages_with_teachers(table, language_combinations):
+    possible_languages = set()
+
+    for combination in language_combinations:
+        if _combination_has_teachers(table, combination):
+            possible_languages.add(combination)
+
+    return possible_languages
+
+
+def _combination_has_teachers(table, combination):
+    has_teacher = {}
+
+    for human in table:
+        for language in human.teaching_languages:
+            has_teacher[language] = True
+            if _combination_has_enough_teachers(has_teacher, combination):
+                return True
+
+    return False
+
+
+def _combination_has_enough_teachers(has_teacher, combination):
+    for language in combination:
+        if not has_teacher.get(language, False):
+            return False
+
+    return True
+
+
+def _get_overlapping_languages(table):
+    table_languages = _get_language_combinations(table[0])
+
+    for human in table[1:]:
+        table_languages &= _get_language_combinations(human)
+        if not table_languages:
+            break
+
+    return table_languages
+
+
+def _get_language_combinations(human):
+    learning_languages = (language for language, _ in human.learning_languages)
+    language_combinations = product(learning_languages, human.teaching_languages)
+    return {frozenset(combination) for combination in language_combinations}
+
+
+def unhappiness(table, language_combination):
+    ranking_unhappiness = 0
+    levels = []
+
+    for human in table:
+        for idx, (learning_language, level) in enumerate(human.learning_languages):
+            if learning_language in language_combination:
+                ranking_unhappiness += idx
+                levels.append(level)
+                break
+    levels = [int(i) for i in levels]
+    levels = np.array(levels)
+    levels = levels / max(levels)
+    level_unhappiness = np.std(levels)
+    total_unhappiness = ranking_unhappiness + level_unhappiness
+    return total_unhappiness
+
+
+def _acceptable_level_difference(table, language_combination):
+    for language in language_combination:
+        levels = _learning_levels(table, language)
+        if _max_difference(levels) > MAX_DIFFERENCE:
+            return False
+
+    return True
+
+
+def _learning_levels(table, language):
+    levels = []
+    for human in table:
+        for learning_language, level in human.learning_languages:
+            if learning_language == language:
+                levels.append(level)
+                break
+    return levels
+
+
+def _max_difference(levels):
+    levels = [int(i) for i in levels]
+    return max(levels) - min(levels)
+    
+
+def optimized_tables(possible_tables, is_seated):
+    optimized_tables = []
+    print("The chosen tables are")
+    for language_table in possible_tables:
+        if is_seated[language_table].value() == 1.0:
+            optimized_tables.append(language_table)
+            print(language_table[0])
+            print(language_table[1])
+    return optimized_tables
+
+
+def make_ilp_model(humans, is_seated, possible_tables):
+    seating_model = pulp.LpProblem("Tandem Seating Model", pulp.LpMinimize)
+    seating_model += pulp.lpSum([unhappiness(*language_table) * is_seated[language_table] 
+                                 for language_table in possible_tables])
+    for human in humans:
+        seating_model += (pulp.lpSum([is_seated[(table, languages)] 
+                                     for table, languages in possible_tables if human in table]) == 1,
+                          "Must_seat_{}".format(human))
+        
+    return seating_model
+
+if __name__ == '__main__':
+    seater = SymmetricSeater(HUMANS, MAX_TABLE_SIZE)
+    print(seater.seat())
--- tandem.py
+++ tandem.py
@@ -1,12 +1,9 @@
-from itertools import product
-
-import numpy as np
+import abc
 import pulp
 
 from humans import Human
 
-MAX_TABLE_SIZE = 4
-MAX_DIFFERENCE = 1
+
 
 HUMANS = [Human(name='anna', learning_languages=[('german', 10)], teaching_languages=['french', 'english']),
           Human(name='bert', learning_languages=[('english', 2), ('french', 2)], teaching_languages=['german']),
@@ -16,155 +13,31 @@ HUMANS = [Human(name='anna', learning_languages=[('german', 10)], teaching_langu
           Human(name='francisca', learning_languages=[('arabic', 2), ('hausa', 2)], teaching_languages=['turkish']),
           ]
 
+MAX_TABLE_SIZE = 4
+MAX_DIFFERENCE = 1
 
-def _table_languages(table):
-    language_combinations = _get_overlapping_languages(table)
-    valid_languages = _languages_with_teachers(table, language_combinations)
-    return valid_languages
-
-
-def _languages_with_teachers(table, language_combinations):
-    possible_languages = set()
-
-    for combination in language_combinations:
-        if _combination_has_teachers(table, combination):
-            possible_languages.add(combination)
-
-    return possible_languages
-
-
-def _combination_has_teachers(table, combination):
-    has_teacher = {}
-
-    for human in table:
-        for language in human.teaching_languages:
-            has_teacher[language] = True
-            if _combination_has_enough_teachers(has_teacher, combination):
-                return True
-
-    return False
-
-
-def _combination_has_enough_teachers(has_teacher, combination):
-    for language in combination:
-        if not has_teacher.get(language, False):
-            return False
-
-    return True
-
-
-def _get_overlapping_languages(table):
-    table_languages = _get_language_combinations(table[0])
-
-    for human in table[1:]:
-        table_languages &= _get_language_combinations(human)
-        if not table_languages:
-            break
-
-    return table_languages
-
-
-def _get_language_combinations(human):
-    learning_languages = (language for language, _ in human.learning_languages)
-    language_combinations = product(learning_languages, human.teaching_languages)
-    return {frozenset(combination) for combination in language_combinations}
-
-
-def unhappiness(table, language_combination):
-    ranking_unhappiness = 0
-    levels = []
-
-    for human in table:
-        for idx, (learning_language, level) in enumerate(human.learning_languages):
-            if learning_language in language_combination:
-                ranking_unhappiness += idx
-                levels.append(level)
-                break
-    levels = [int(i) for i in levels]
-    levels = np.array(levels)
-    levels = levels / max(levels)
-    level_unhappiness = np.std(levels)
-    total_unhappiness = ranking_unhappiness + level_unhappiness
-    return total_unhappiness
-
-
-def language_tables(humans, max_table_size):
-    for table in pulp.allcombinations(humans, max_table_size):
-        if len(table) <= 1:
-            continue
-        languages = _table_languages(table)
-        for language_combination in languages:
-            if _acceptable_level_difference(table, language_combination):
-                yield (table, frozenset(language_combination))
-
-
-def _acceptable_level_difference(table, language_combination):
-    for language in language_combination:
-        levels = _learning_levels(table, language)
-        if _max_difference(levels) > MAX_DIFFERENCE:
-            return False
-
-    return True
-
-
-def _learning_levels(table, language):
-    levels = []
-    for human in table:
-        for learning_language, level in human.learning_languages:
-            if learning_language == language:
-                levels.append(level)
-                break
-    return levels
-
-
-def _max_difference(levels):
-    levels = [int(i) for i in levels]
-    return max(levels) - min(levels)
-
-
-def calculate_tables(humans, max_table_size):
-    possible_tables = list(language_tables(humans, max_table_size))
-    is_seated = pulp.LpVariable.dicts('table',
-                                      possible_tables,
-                                      lowBound=0,
-                                      upBound=1,
-                                      cat=pulp.LpInteger)
-    seating_model = make_ilp_model(humans, is_seated, possible_tables)
-    seating_model.solve()
+class Seater(abc.ABC):
     
-    return optimized_tables(possible_tables, is_seated)
-    
-    
-def get_seatings(all_humans, max_table_size):
-    results = calculate_tables(all_humans, max_table_size)
-    seated_humans = set()
-    for humans, _ in results:
-        seated_humans.update(humans)
-    not_matched = [human for human in all_humans if human not in seated_humans]
-    return results, not_matched
-    
-
-def optimized_tables(possible_tables, is_seated):
-    optimized_tables = []
-    print("The chosen tables are")
-    for language_table in possible_tables:
-        if is_seated[language_table].value() == 1.0:
-            optimized_tables.append(language_table)
-            print(language_table[0])
-            print(language_table[1])
-    return optimized_tables
-
-
-def make_ilp_model(humans, is_seated, possible_tables):
-    seating_model = pulp.LpProblem("Tandem Seating Model", pulp.LpMinimize)
-    seating_model += pulp.lpSum([unhappiness(*language_table) * is_seated[language_table] 
-                                 for language_table in possible_tables])
-    for human in humans:
-        seating_model += (pulp.lpSum([is_seated[(table, languages)] 
-                                     for table, languages in possible_tables if human in table]) == 1,
-                          "Must_seat_{}".format(human))
+    def __init__(self, humans, max_table_size):
+        self.humans = humans
+        self.max_table_size = max_table_size
         
-    return seating_model
+    def seat(self):
+        seatings = self._optimal_seatings()
+        seated_humans = set()
+        for humans, _ in seatings:
+            seated_humans.update(humans)
+        not_matched = [human for human in self.humans if human not in seated_humans]
+        return seatings, not_matched
+    
+    @abc.abstractmethod
+    def _optimal_seatings(self):
+        ...
 
-if __name__ == '__main__':
-    get_seatings(HUMANS, MAX_TABLE_SIZE)
+    def _tables(self):
+        tables = pulp.allcombinations(self.humans, self.max_table_size)
+        return [table for table in tables if len(table) > 1]
+    
+    @abc.abstractmethod
+    def _filtered_tables(self):
+        ...
