549498f0dd1cb303990c2e7413709e773689d20a
DoHe <wahlholz@gmx.de>
2015-11-01 13:34:15 +0100

    Profiles for memory and time consumption

--- tandem/asymmetric_tandem.py
+++ tandem/asymmetric_tandem.py
@@ -20,7 +20,6 @@ class AsymmetricSeater(Seater):
             language_combination = frozenset((language,))
             yield table, language_combination
 
-
     def _optimal_seatings(self, possible_tables):
         possible_tables = list(set(possible_tables))
         impossible_humans = set()
@@ -95,45 +94,50 @@ def _ranking_unhappiness(table, language_combination):
     return ranking_unhappiness
 
 
+def _ilp_variables(human, possible_tables, is_seated_ilp):
+    round1_variables = []
+    round2_variables = []
+    pupil_variables = []
+    teacher_variables = []
+    
+    for table_1, table_2 in possible_tables:
+        if human in table_1[0]:
+            round1_variables.append(is_seated_ilp[(table_1, table_2)])
+            
+            if _is_pupil(human, table_1[1]):
+                pupil_variables.append(is_seated_ilp[(table_1, table_2)])
+                
+            if _is_teacher(human, table_1[1]):
+                teacher_variables.append(is_seated_ilp[(table_1, table_2)])
+            
+        if human in table_2[0]:
+            round2_variables.append(is_seated_ilp[(table_1, table_2)])
+            
+            if _is_pupil(human, table_2[1]):
+                pupil_variables.append(is_seated_ilp[(table_1, table_2)])
+                
+            if _is_teacher(human, table_2[1]):
+                teacher_variables.append(is_seated_ilp[(table_1, table_2)])
+                
+    return round1_variables, round2_variables, pupil_variables, teacher_variables
+                
+                
 def _make_ilp_model(humans, is_seated_ilp, possible_tables):
     seating_model = pulp.LpProblem("Tandem Seating Model", pulp.LpMinimize)
     seating_model += pulp.lpSum([_unhappiness(*language_table) * is_seated_ilp[language_table]
                                  for language_table in possible_tables])
 
-
     for human in humans:
-        round1_variables = []
-        for table_1, table_2 in possible_tables:
-            if human in table_1[0]:
-                round1_variables.append(is_seated_ilp[(table_1, table_2)])
-
+        round1_variables, round2_variables, pupil_variables, teacher_variables = _ilp_variables(human,
+                                                                                                possible_tables,
+                                                                                                is_seated_ilp)
         seating_model += (pulp.lpSum(round1_variables) == 1,
                           "Must_seat_exatcly_once_round1_{}".format(human))
 
-
-        round2_variables = []
-        for table_1, table_2 in possible_tables:
-            if human in table_2[0]:
-                round2_variables.append(is_seated_ilp[(table_1, table_2)])
-
         seating_model += (pulp.lpSum(round2_variables) == 1,
                           "Must_seat_exatcly_once_round2_{}".format(human))
-
-
-        pupil_variables = []
-        for table_1, table_2 in possible_tables:
-            if human in table_1[0] and _is_pupil(human, table_1[1]) or human in table_2[0] and _is_pupil(human, table_2[1]):
-                pupil_variables.append(is_seated_ilp[(table_1, table_2)])
-
         seating_model += (pulp.lpSum(pupil_variables) == 1,
                           "Must_seat_as_pupil_{}".format(human))
-
-
-        teacher_variables = []
-        for table_1, table_2 in possible_tables:
-            if human in table_1[0] and _is_teacher(human, table_1[1]) or human in table_2[0] and _is_teacher(human, table_2[1]):
-                teacher_variables.append(is_seated_ilp[(table_1, table_2)])
-
         seating_model += (pulp.lpSum(teacher_variables) == 1,
                           "Must_seat_as_teacher_{}".format(human))
 
--- tandem/base_tandem.py
+++ tandem/base_tandem.py
@@ -10,10 +10,18 @@ HUMANS = [Human(name='anna', learning_languages=[('german', 10)], teaching_langu
           Human(name='clara', learning_languages=[('german', 2), ('english', 2)], teaching_languages=['french']),
           Human(name='dirk', learning_languages=[('german', 2), ('english', 2)], teaching_languages=['french']),
           Human(name='erik', learning_languages=[('greek', 2), ('french', 2)], teaching_languages=['german']),
-          Human(name='francisca', learning_languages=[('arabic', 2), ('hausa', 2)], teaching_languages=['turkish']),
+          Human(name='na', learning_languages=[('german', 10)], teaching_languages=['french', 'english']),
+          Human(name='nb', learning_languages=[('english', 2), ('french', 2)], teaching_languages=['german']),
+          Human(name='nc', learning_languages=[('german', 2), ('english', 2)], teaching_languages=['french']),
+          Human(name='nd', learning_languages=[('german', 2), ('english', 2)], teaching_languages=['french']),
+          Human(name='ne', learning_languages=[('greek', 2), ('french', 2)], teaching_languages=['german']),
+          Human(name='aa', learning_languages=[('german', 10)], teaching_languages=['french', 'english']),
+          Human(name='ab', learning_languages=[('english', 2), ('french', 2)], teaching_languages=['german']),
+          Human(name='ac', learning_languages=[('german', 2), ('english', 2)], teaching_languages=['french']),
+          Human(name='ad', learning_languages=[('german', 2), ('english', 2)], teaching_languages=['french']),
           ]
 
-MAX_TABLE_SIZE = 4
+MAX_TABLE_SIZE = 3
 MAX_DIFFERENCE = 1
 
 class Seater(abc.ABC):
